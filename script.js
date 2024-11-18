21b03ee9bb96bafba28588a9a9271e7ecbdf9f64
elisasilva06 <ana.eosilva@aluno.obompastor.com.br>
2024-11-18 19:55:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Abre o modal
+function openModal(id) {
+    document.getElementById(id).style.display = "flex";
+  }
+  
+  // Fecha o modal
+  function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+  }
+
